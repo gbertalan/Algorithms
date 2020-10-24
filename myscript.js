9e0521984bf553837df1a39c512afdaef0871cdd
Gergely Bertalan <bertalanofficial@gmail.com>
2020-10-24 20:01:45 +0200

    Implement randomization of the integers for the bubble sort

--- myscript.js
+++ myscript.js
@@ -3,11 +3,11 @@ function randomize() {
     let child = document.getElementById("#bubble-sort-random-button");
     let parent = child.parentNode;
     let negyzet = parent.children[1].children;
-    negyzet[4].style = "background: blue";
-    console.log(" igen " + i);
-    ++i;
 
-    let text = (Math.random() * 100) - 50;
-    text = Math.floor(text);
-    negyzet[5].value = text;
+    for (let index = 0; index < negyzet.length - 1; index++) {
+        let text = (Math.random() * 100) - 50;
+        text = Math.floor(text);
+        negyzet[index].value = text;
+    }
+
 }
